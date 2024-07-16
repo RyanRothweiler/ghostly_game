c7d5f24decd62e96fca9a5abbb152e5fbee17eac
RyanRothweiler <ryanrothweiler@gmail.com>
2024-07-15 22:47:18 -0700

    windows shenanigans

--- ghostly_game_windows/src/main.rs
+++ ghostly_game_windows/src/main.rs
@@ -5,6 +5,8 @@ use gengar_render_opengl::render;
 
 use ghostly_game::game;
 
+use windows::Win32::Graphics::Gdi::*;
+use windows::Win32::Graphics::OpenGL::*;
 use windows::{
     core::*, Win32::Foundation::*, Win32::Graphics::Gdi::ValidateRect,
     Win32::System::LibraryLoader::GetModuleHandleA, Win32::UI::WindowsAndMessaging::*,
@@ -40,14 +42,15 @@ fn main() {
             lpszClassName: window_class,
 
             style: CS_HREDRAW | CS_VREDRAW,
-            lpfnWndProc: Some(wndproc),
+            lpfnWndProc: Some(windows_callback),
             ..Default::default()
         };
 
         let atom = RegisterClassA(&wc);
         debug_assert!(atom != 0);
 
-        CreateWindowExA(
+        // create main window
+        let main_window_handle = CreateWindowExA(
             WINDOW_EX_STYLE::default(),
             window_class,
             s!("Ghostly"),
@@ -61,6 +64,87 @@ fn main() {
             instance,
             None,
         );
+
+        // Use dummy device context to get the proc addresses needed for the final window
+        {
+            let dummy_window_class: PCSTR = s!("dummy_window");
+
+            let dummy_wc = WNDCLASSA {
+                hCursor: LoadCursorW(None, IDC_ARROW).unwrap(),
+                hInstance: instance.into(),
+                lpszClassName: dummy_window_class,
+
+                style: CS_HREDRAW | CS_VREDRAW,
+                lpfnWndProc: Some(dummy_windows_callback),
+                ..Default::default()
+            };
+
+            let dummy_atom = RegisterClassA(&dummy_wc);
+            debug_assert!(dummy_atom != 0);
+
+            let dummy_win_handle = CreateWindowExA(
+                WINDOW_EX_STYLE::default(),
+                dummy_window_class,
+                s!("ghostly_dummy"),
+                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                None,
+                None,
+                instance,
+                None,
+            );
+
+            let dummy_device_context = GetDC(dummy_win_handle);
+
+            let nsize = std::mem::size_of::<PIXELFORMATDESCRIPTOR>() as u16;
+            let dummy_desired_pixel_format: PIXELFORMATDESCRIPTOR = PIXELFORMATDESCRIPTOR {
+                nSize: nsize,
+                nVersion: 1,
+                dwFlags: PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW,
+                iPixelType: PFD_TYPE_RGBA,
+                cColorBits: 32,
+                cDepthBits: 24,
+                cStencilBits: 8,
+
+                ..Default::default()
+            };
+
+            let result: i32 = ChoosePixelFormat(dummy_device_context, &dummy_desired_pixel_format);
+            if result == 0 {
+                println!("Invalid pixel format");
+            }
+
+            let suggested_pixel_format: PIXELFORMATDESCRIPTOR = PIXELFORMATDESCRIPTOR {
+                ..Default::default()
+            };
+            let suggested_pixel_format_index: i32 =
+                ChoosePixelFormat(dummy_device_context, &suggested_pixel_format);
+            if suggested_pixel_format_index == 0 {
+                println!("Invalid suggested pixel format");
+            }
+
+            SetPixelFormat(
+                dummy_device_context,
+                suggested_pixel_format_index,
+                &dummy_desired_pixel_format,
+            )
+            .unwrap();
+
+            let dummy_opengl_context = wglCreateContext(dummy_device_context).unwrap();
+            wglMakeCurrent(dummy_device_context, dummy_opengl_context).unwrap();
+
+            // get proc addresses here
+
+            //wglDeleteContext(DummyOpenGLRC);
+            ReleaseDC(dummy_win_handle, dummy_device_context);
+            DestroyWindow(dummy_win_handle).unwrap();
+        }
+
+        // init opengl
+        let device_context = GetDC(main_window_handle);
 
         let mut message = MSG::default();
 
@@ -87,7 +171,12 @@ fn main() {
     }
 }
 
-extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+extern "system" fn windows_callback(
+    window: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
     unsafe {
         match message {
             WM_PAINT => {
@@ -103,4 +192,13 @@ extern "system" fn wndproc(window: HWND, message: u32, wparam: WPARAM, lparam: L
             _ => DefWindowProcA(window, message, wparam, lparam),
         }
     }
+}
+
+extern "system" fn dummy_windows_callback(
+    window: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
+    unsafe { DefWindowProcA(window, message, wparam, lparam) }
 }
