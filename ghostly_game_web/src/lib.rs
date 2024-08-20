a086ae43024780395934bf1d358351f5209cc015
RyanRothweiler <ryanrothweiler@gmail.com>
2024-08-19 17:02:45 -0700

    wasm loop working

--- ghostly_game_web/src/lib.rs
+++ ghostly_game_web/src/lib.rs
@@ -1,13 +1,26 @@
+#![allow(unused_variables, unused_imports)]
+
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
 
+pub fn log(input: &str) {
+    console::log_1(&input.into());
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {}
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("hello there");
+pub fn main_loop() {
+    log("main loop");
 }
