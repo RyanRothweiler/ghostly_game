5f9aac8b5fe7286839e9b09b08d3a13ce62324b3
RyanRothweiler <ryanrothweiler@gmail.com>
2024-08-20 15:02:41 -0700

    web full screen gl clear

--- ghostly_game_web/src/lib.rs
+++ ghostly_game_web/src/lib.rs
@@ -3,7 +3,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use web_sys::{console, WebGl2RenderingContext, WebGlProgram, WebGlShader};
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -22,5 +22,25 @@ pub fn start() {}
 
 #[wasm_bindgen]
 pub fn main_loop() {
+    let document = web_sys::window().unwrap().document().unwrap();
+
+    let canvas = document.get_element_by_id("gengar_canvas").unwrap();
+
+    let canvas: web_sys::HtmlCanvasElement =
+        canvas.dyn_into::<web_sys::HtmlCanvasElement>().unwrap();
+
+    let gl_context = canvas
+        .get_context("webgl2")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<WebGl2RenderingContext>()
+        .unwrap();
+
+    // gl_context.canvas().unwrap().width = 100;
+
+    gl_context.viewport(0, 0, 1000, 1000);
+    gl_context.clear_color(1.0, 0.0, 0.0, 1.0);
+    gl_context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
     log("main loop");
 }
--- ghostly_game_web/www/index.js
+++ ghostly_game_web/www/index.js
@@ -6,3 +6,12 @@ const renderLoop = () => {
 };
 
 requestAnimationFrame(renderLoop);
+
+const canvas = document.getElementById("gengar_canvas");
+var gl = canvas.getContext("webgl2");
+
+var width = gl.canvas.clientWidth;
+var height = gl.canvas.clientHeight;
+
+gl.canvas.width = width;
+gl.canvas.height = height;
