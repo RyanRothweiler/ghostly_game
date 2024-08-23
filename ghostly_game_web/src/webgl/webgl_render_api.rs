b6c397807ebf42895f7eba69473c358064ac5d07
RyanRothweiler <ryanrothweiler@gmail.com>
2024-08-23 16:56:09 -0700

    starting shader compilation

--- ghostly_game_web/src/webgl/webgl_render_api.rs
+++ ghostly_game_web/src/webgl/webgl_render_api.rs
@@ -21,10 +21,10 @@ pub struct WebGLRenderApi {
     pub gl_clear_color: fn(f32, f32, f32, f32),
     pub gl_clear: fn(),
     pub gl_create_program: fn() -> Option<WebGlProgram>,
+    pub gl_create_shader: fn(u32) -> Option<WebGlShader>,
+    pub gl_shader_source: fn(&WebGlShader, &str),
+    pub gl_compile_shader: fn(&WebGlShader),
     /*
-    pub gl_create_shader: fn(i32) -> u32,
-    pub gl_shader_source: fn(u32, &str),
-    pub gl_compile_shader: fn(u32),
     pub gl_get_shader_iv: fn(u32, i32, *mut i32),
     pub gl_shader_info_log: fn(u32, i32, *mut i32, &mut Vec<u8>),
     pub gl_attach_shader: fn(u32, u32),
@@ -48,6 +48,54 @@ pub fn get_render_api() -> WebGLRenderApi {
         gl_clear_color: gl_clear_color,
         gl_clear: gl_clear,
         gl_create_program: gl_create_program,
+        gl_create_shader: gl_create_shader,
+        gl_shader_source: gl_shader_source,
+        gl_compile_shader: gl_compile_shader,
+    }
+}
+
+impl WebGLRenderApi {
+    /*
+    fn shader_info_log(&self, id: u32) -> Result<String, EngineError> {
+        let mut string_buf: Vec<u8> = vec![0; 1024];
+
+        let mut output_len: i32 = 0;
+        (self.gl_shader_info_log)(id, 1024, &mut output_len, &mut string_buf);
+
+        let error: String = std::ffi::CStr::from_bytes_until_nul(string_buf.as_ref())?
+            .to_str()?
+            .to_string();
+
+        return Ok(error);
+    }
+    */
+
+    fn compile_shader(
+        &self,
+        shader_source: &str,
+        shader_type: ShaderType,
+    ) -> Result<WebGlShader, EngineError> {
+        let gl_shader_type: u32 = match shader_type {
+            ShaderType::Vertex => WebGl2RenderingContext::VERTEX_SHADER,
+            ShaderType::Fragment => WebGl2RenderingContext::FRAGMENT_SHADER,
+        };
+
+        let shader: WebGlShader = (self.gl_create_shader)(gl_shader_type).unwrap();
+        (self.gl_shader_source)(&shader, shader_source);
+        (self.gl_compile_shader)(&shader);
+
+        /*
+        let mut status: i32 = -1;
+        (self.gl_get_shader_iv)(id, GL_COMPILE_STATUS, &mut status);
+        if status == GL_FALSE {
+            let error_info: String = self.shader_info_log(id)?;
+            return Err(EngineError::ShaderCompilation(error_info));
+        }
+
+        Ok(id)
+        */
+
+        Ok(shader)
     }
 }
 
@@ -59,13 +107,13 @@ impl EngineRenderApiTrait for WebGLRenderApi {
     ) -> Result<u32, EngineError> {
         let prog: WebGlProgram = (self.gl_create_program)().unwrap();
 
+        let vert_shader = self.compile_shader(vert_shader, ShaderType::Vertex)?;
+
         /*
-        let vert_id = self.compile_shader(vert_shader, ShaderType::Vertex)?;
         let frag_id = self.compile_shader(frag_shader, ShaderType::Fragment)?;
 
         (self.gl_attach_shader)(prog_id, vert_id);
-        (self.gl_attach_shader)(prog_id, frag_id);    gl_clear: clear,
-
+        (self.gl_attach_shader)(prog_id, frag_id);
 
         let mut status: i32 = -1;
         (self.gl_get_shader_iv)(prog_id, GL_LINK_STATUS, &mut status);
@@ -134,5 +182,23 @@ pub fn gl_clear() {
 pub fn gl_create_program() -> Option<WebGlProgram> {
     unsafe {
         return (GL_CONTEXT.as_mut().unwrap()).create_program();
+    }
+}
+
+pub fn gl_create_shader(ty: u32) -> Option<WebGlShader> {
+    unsafe {
+        return (GL_CONTEXT.as_mut().unwrap()).create_shader(ty);
+    }
+}
+
+pub fn gl_shader_source(shader: &WebGlShader, source: &str) {
+    unsafe {
+        return (GL_CONTEXT.as_mut().unwrap()).shader_source(shader, source);
+    }
+}
+
+pub fn gl_compile_shader(shader: &WebGlShader) {
+    unsafe {
+        return (GL_CONTEXT.as_mut().unwrap()).compile_shader(shader);
     }
 }
