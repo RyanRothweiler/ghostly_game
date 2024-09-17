52d0998a98c92e8123557d09818056a205f9e958
RyanRothweiler <ryanrothweiler@gmail.com>
2024-09-16 23:29:05 -0700

    fly cam working!

--- gengar_engine/src/render/camera.rs
+++ gengar_engine/src/render/camera.rs
@@ -8,8 +8,9 @@ pub enum ProjectionType {
 }
 
 pub struct Camera {
-    pub euler_rotation: VecThreeFloat,
     pub forward: VecThreeFloat,
+    pub yaw: f64,
+    pub pitch: f64,
 
     pub transform: Transform,
     pub view_mat: M44,
@@ -32,7 +33,6 @@ impl Camera {
             view_mat: M44::new_identity(),
             projection_mat: M44::new_identity(),
 
-            euler_rotation: VecThreeFloat::new_zero(),
             forward: VecThreeFloat::new(0.0, 0.0, 1.0),
 
             resolution,
@@ -41,6 +41,9 @@ impl Camera {
             near_plane: 0.1,
             far_plane: 10.0,
             fov: 0.0,
+
+            yaw: 90.0,
+            pitch: 0.0,
         }
     }
 
@@ -66,137 +69,85 @@ impl Camera {
             }
         }
 
-        let up = VecThreeFloat::new(0.0, 1.0, 0.0);
-
-        /*
-        // broken fly cam attempt
+        // view matrix
         {
-            let rot_y: M44 = M44::new_rotation_y(self.euler_rotation.x * -1.0);
-            self.forward = M44::apply_vec_three(&rot_y, &VecThreeFloat::new(0.0, 0.0, 1.0));
-            let rot_z: M44 = M44::new_rotation_x(self.euler_rotation.y * -1.0);
-            self.forward = M44::apply_vec_three(&rot_z, &self.forward);
+            let up = VecThreeFloat::new(0.0, 1.0, 0.0);
 
-            let look_target_position = self.transform.position + (self.forward * -1.0);
+            // Cam yaw / pitch axis
+            self.forward = VecThreeFloat::new_zero();
+            self.forward.x = self.yaw.to_radians().cos() * self.pitch.to_radians().cos();
+            self.forward.y = self.pitch.to_radians().sin();
+            self.forward.z = self.yaw.to_radians().sin() * self.pitch.to_radians().cos();
+            self.forward.normalize();
 
-            // z
-            let mut z_axis = self.transform.position - look_target_position;
-            z_axis.normalize();
+            let target_pos = self.transform.position + (self.forward * -1.0);
 
-            // x
-            let mut x_axis = VecThreeFloat::cross(up, z_axis);
-            x_axis.normalize();
+            let mut cam_dir = self.transform.position - target_pos;
+            cam_dir.normalize();
 
-            // y
-            let _y_axis = VecThreeFloat::cross(z_axis, x_axis);
+            let mut cam_right = VecThreeFloat::cross(up, cam_dir);
+            cam_right.normalize();
 
+            let cam_up = VecThreeFloat::cross(cam_dir, cam_right);
+
+            // Setup matrix
+            self.view_mat = M44::new_identity();
+
+            let inv_pos = VecThreeFloat::new(
+                -self.transform.position.x,
+                -self.transform.position.y,
+                -self.transform.position.z,
+            );
+
+            self.view_mat.set(0, 0, cam_right.x);
+            self.view_mat.set(1, 0, cam_right.y);
+            self.view_mat.set(2, 0, cam_right.z);
+
+            self.view_mat.set(0, 1, cam_up.x);
+            self.view_mat.set(1, 1, cam_up.y);
+            self.view_mat.set(2, 1, cam_up.z);
+
+            self.view_mat.set(0, 2, cam_dir.x);
+            self.view_mat.set(1, 2, cam_dir.y);
+            self.view_mat.set(2, 2, cam_dir.z);
+
+            self.view_mat.translate(inv_pos);
         }
-        */
-
-        let rot_y: M44 = M44::new_rotation_y(self.euler_rotation.x * -1.0);
-        self.forward = M44::apply_vec_three(&rot_y, &VecThreeFloat::new(0.0, 0.0, 1.0));
-        let rot_z: M44 = M44::new_rotation_x(self.euler_rotation.y * -1.0);
-        self.forward = M44::apply_vec_three(&rot_z, &self.forward);
-
-        let target_pos = self.transform.position + (self.forward * -1.0);
-        // let target_pos = VecThreeFloat::new(0.0, 0.0, 0.0);
-
-        println!("{:?}", target_pos);
-
-        draw_sphere(target_pos, 0.1, Color::new(0.0, 1.0, 0.0, 1.0));
-
-        let mut cam_dir = self.transform.position - target_pos;
-        cam_dir.normalize();
-
-        let mut cam_right = VecThreeFloat::cross(up, cam_dir);
-        cam_right.normalize();
-
-        let cam_up = VecThreeFloat::cross(cam_dir, cam_right);
-
-        // View matrix
-        self.view_mat = M44::new_identity();
-
-        let inv_pos = VecThreeFloat::new(
-            -self.transform.position.x,
-            -self.transform.position.y,
-            -self.transform.position.z,
-        );
-
-        self.view_mat.set(0, 0, cam_right.x);
-        self.view_mat.set(1, 0, cam_right.y);
-        self.view_mat.set(2, 0, cam_right.z);
-
-        self.view_mat.set(0, 1, cam_up.x);
-        self.view_mat.set(1, 1, cam_up.y);
-        self.view_mat.set(2, 1, cam_up.z);
-
-        self.view_mat.set(0, 2, cam_dir.x);
-        self.view_mat.set(1, 2, cam_dir.y);
-        self.view_mat.set(2, 2, cam_dir.z);
-
-        self.view_mat.translate(inv_pos);
-
-        /*
-        self.view_mat.set(0, 0, x_axis.x);
-        self.view_mat.set(0, 1, x_axis.y);
-        self.view_mat.set(0, 2, x_axis.z);
-
-        self.view_mat.set(1, 0, y_axis.x);
-        self.view_mat.set(1, 1, y_axis.y);
-        self.view_mat.set(1, 2, y_axis.z);
-
-        self.view_mat.set(2, 0, z_axis.x);
-        self.view_mat.set(2, 1, z_axis.y);
-        self.view_mat.set(2, 2, z_axis.z);
-        */
     }
 
     // Control the camera as a fly-cam
     // Mouse for rotation and wasd for camera relative movement
-    pub fn move_fly_(&mut self, speed: f64, input: &Input) {
+    pub fn move_fly_(&mut self, mov_speed: f64, input: &Input) {
         if input.mouse_right.pressing {
-            // println!("{:?}", self.euler_rotation);
-            self.euler_rotation.x = self.euler_rotation.x + (input.mouse_pos_delta.x * 0.001);
-            self.euler_rotation.y = self.euler_rotation.y + (input.mouse_pos_delta.y * 0.001);
+            let sens = 0.08;
+            self.yaw = self.yaw - (input.mouse_pos_delta.x * sens);
+            self.pitch = self.pitch - (input.mouse_pos_delta.y * sens);
         }
 
-        println!("{:?}", self.transform.position);
+        let mut right = VecThreeFloat::cross(self.forward, VecThreeFloat::new(0.0, 1.0, 0.0));
+        right.normalize();
 
-        let right = VecThreeFloat::cross(self.forward, VecThreeFloat::new(0.0, 1.0, 0.0));
-        let up = VecThreeFloat::cross(self.forward, VecThreeFloat::new(1.0, 0.0, 0.0));
+        let mut up = VecThreeFloat::cross(self.forward, right);
+        up.normalize();
 
         if input.keyboard[ASCII_A].pressing {
-            self.transform.position = self.transform.position + (right * speed);
+            self.transform.position = self.transform.position + (right * mov_speed);
         }
         if input.keyboard[ASCII_D].pressing {
-            self.transform.position = self.transform.position - (right * speed);
+            self.transform.position = self.transform.position - (right * mov_speed);
         }
         if input.keyboard[ASCII_S].pressing {
-            self.transform.position = self.transform.position + (self.forward * speed);
+            self.transform.position = self.transform.position + (self.forward * mov_speed);
         }
         if input.keyboard[ASCII_W].pressing {
-            self.transform.position = self.transform.position - (self.forward * speed);
+            self.transform.position = self.transform.position - (self.forward * mov_speed);
         }
         if input.keyboard[ASCII_Q].pressing {
-            self.transform.position = self.transform.position + (up * speed);
+            self.transform.position = self.transform.position + (up * mov_speed);
         }
         if input.keyboard[ASCII_E].pressing {
-            self.transform.position = self.transform.position - (up * speed);
+            self.transform.position = self.transform.position - (up * mov_speed);
         }
-
-        /*
-        if input.keyboard[ASCII_A].pressing {
-            self.forward.x = self.forward.x - speed;
-        }
-        if input.keyboard[ASCII_D].pressing {
-            self.forward.x = self.forward.x + speed;
-        }
-        if input.keyboard[ASCII_S].pressing {
-            self.forward.y = self.forward.y - speed;
-        }
-        if input.keyboard[ASCII_W].pressing {
-            self.forward.y = self.forward.y + speed;
-        }
-        */
 
         self.update_matricies();
     }
